4db85fed608f4d7ef2cca92c51e9e2ce645729e7
Karim Chmayssani <karimchmay@gmail.com>
2024-11-14 20:13:38 -0500

    Have all events tracked, need to figure out general scoring system

--- accuracy/accuracy.py
+++ accuracy/accuracy.py
@@ -4,18 +4,69 @@ import argparse as ag
 import numpy as np
 import pandas as pd
 
-stimulus = {
-	'CenterMO_IN': 0,
-	'CenterMO_OUT': 0,
-	'LeftMO_IN': 0,
-	'LeftMO_OUT': 0,
-	'RightMO_IN': 0,
-	'RightMO_OUT': 0,
-}
-
 argv = ag.ArgumentParser()
 argv.add_argument('input_file', type=str)
 argv.add_argument('output_file', type=str, default='*/')
+
+class accuracy:
+
+	def __init__(self, data):
+		self.data = data
+		self.point = ['Center', 'Left', 'Right']
+		self.dir = ['In', 'Out']
+		self.totals = {}
+
+		for p in self.point:
+			for d in self.dir:
+				e = p + d
+				self.totals[e] = 0
+
+		self.find_summary()
+
+	def find_summary(self):
+		idx = self.data.index[self.data['0'] == 'SUMMARY DATA'][0]
+		summary = self.data.iloc[idx:,:9].reset_index(drop=True)
+
+		idx = summary.index[summary['0'] == 'Condition'][0]
+		self.data = summary.iloc[idx:].reset_index(drop=True)
+
+		self.data.columns = self.data.iloc[0]
+		self.data = self.data.iloc[1:].reset_index(drop=True)
+
+	def sum_stimulus(self, idx):
+		event = self.data['Stimulus'][idx]
+		presses = {}
+
+		for p in self.point:
+			if p in event:
+				type = p
+				break
+		for d in self.dir:
+			if d in event:
+				direction = d
+		event = type + direction
+
+		n_events = self.data['n Events'][idx]
+		n_c_event = 0
+
+		i = 1
+		print(event)
+
+		while n_c_event != n_events:
+			n_c_event = self.data['n Events'][idx+i]
+			presses[i] = n_c_event
+			i = i + 1
+
+		print(presses)
+		if type == 'Center':
+			pass
+
+		self.totals[event] = self.totals[event] + n_events 
+
+	def accuracy(self):
+		for i in range(len(self.data['Stimulus'])):
+			if type(self.data['Stimulus'][i]) == str:
+				self.sum_stimulus(i)
 
 def main(args):
 	i_path = args.input_file
@@ -23,17 +74,12 @@ def main(args):
 	cols = np.array(range(data.shape[1]), dtype=str)
 	data.columns = cols
 
-	idx = data.index[data['0'] == 'SUMMARY DATA'][0]
-	summary = data.iloc[idx:,:9].reset_index(drop=True)
+	acc = accuracy(data)
 
-	idx = summary.index[summary['0'] == 'Condition'][0]
-	print(idx)
-	data = summary.iloc[idx:].reset_index(drop=True)
+	acc.accuracy()
 
-	data.columns = data.iloc[0]
-	data = data.iloc[1:]
+	print(acc.totals)
 
-	print(data.head())
 if __name__ == '__main__':
 	args = argv.parse_args()
 	main(args)
